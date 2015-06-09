d9f430baaf489a29209c31a72de83dd3e5344faa
kellyyu5195 <keyu@vassar.edu>
2015-06-08 23:24:50 -0400

    Uploading Completed Unity Tutorials

---
+++ Unity_SpaceShooter/SpaceShooter Kelly/Assets/Scripts/RandomRotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotator : MonoBehaviour {
+
+	public float tumble;
+	public Rigidbody rb;
+
+	void Start () {
+		rb.angularVelocity = Random.insideUnitSphere * tumble;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
